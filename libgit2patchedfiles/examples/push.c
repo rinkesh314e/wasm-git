0fb7bb5d8702dc1243584a26601090b1510928c5
rinkesh314e <rinkesh.agrawal@314ecorp.com>
2024-04-27 02:15:10 +0530

    add push with params

--- libgit2patchedfiles/examples/push.c
+++ libgit2patchedfiles/examples/push.c
@@ -26,37 +26,44 @@
  * This does have:
  *
  * - Example of push to origin/master
- * 
+ *
  */
 
 /** Entry point for this command */
-int lg2_push(git_repository *repo, int argc, char **argv) {
+int lg2_push(git_repository *repo, int argc, char **argv)
+{
 	git_push_options options;
-	git_remote* remote = NULL;
-	char *refspec = NULL;
-	git_reference* head_ref;
-	
-	git_reference_lookup(&head_ref, repo, "HEAD");
-	refspec = git_reference_symbolic_target(head_ref);
+	git_remote_callbacks callbacks;
+	git_remote *remote = NULL;
+	char *local_branch;
+	char *remote_branch;
+	char refspec_buffer[1024];
 
-	const git_strarray refspecs = {
-		&refspec,
-		1
-	};
-
-    /* Validate args */
-	if (argc > 1) {
-		printf ("USAGE: %s\n\nsorry, no arguments supported yet\n", argv[0]);
+	if (argc < 3)
+	{
+		printf("USAGE: %s <local_branch> <remote_branch>\n", argv[0]);
 		return -1;
 	}
 
-	check_lg2(git_remote_lookup(&remote, repo, "origin" ), "Unable to lookup remote", NULL);
-	
-	check_lg2(git_push_options_init(&options, GIT_PUSH_OPTIONS_VERSION ), "Error initializing push", NULL);
+	local_branch = argv[1];
+	remote_branch = argv[2];
+	snprintf(refspec_buffer, sizeof(refspec_buffer), "refs/heads/%s:refs/heads/%s", local_branch, remote_branch);
+
+	char *refspec = refspec_buffer;
+	const git_strarray refspecs = {
+			&refspec,
+			1};
+
+	check_lg2(git_remote_lookup(&remote, repo, "origin"), "Unable to lookup remote", NULL);
+
+	check_lg2(git_remote_init_callbacks(&callbacks, GIT_REMOTE_CALLBACKS_VERSION), "Error initializing remote callbacks", NULL);
+	callbacks.credentials = cred_acquire_cb;
+
+	check_lg2(git_push_options_init(&options, GIT_PUSH_OPTIONS_VERSION), "Error initializing push", NULL);
+	options.callbacks = callbacks;
 
 	check_lg2(git_remote_push(remote, &refspecs, &options), "Error pushing", NULL);
 
-	printf("pushed\n");
-	git_reference_free(head_ref);
+	printf("Pushed from %s to %s\n", local_branch, remote_branch);
 	return 0;
 }
